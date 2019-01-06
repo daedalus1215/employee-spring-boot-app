011c82aeeaa95e6c5d807211524c4f29df1e0b02
larry <ladams1776@yahoo.com>
2019-01-06 10:42:04 -0500

    got the /api/employees url responding

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDaoHibernateImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDaoHibernateImpl.java
@@ -2,10 +2,13 @@ package com.luv2code.springboot.cruddemo.dao;
 
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -27,7 +30,17 @@ public class EmployeeDaoHibernateImpl implements EmployeeDAO{
     // Implemented methods
 
     @Override
+    @Transactional
     public List<Employee> findAll() {
-        return null;
+
+        // get the current hibernate session
+        Session currentSession = entityManager.unwrap(Session.class);
+
+        // create a query
+        Query<Employee> theQuery =
+                currentSession.createQuery("from Employee", Employee.class);
+
+        // execute query and get result list - then return
+       return theQuery.getResultList();
     }
 }
