312e9b2b0ffa58176118d1ffb6bf51085d48cb6c
larry <ladams1776@yahoo.com>
2019-01-08 08:23:31 -0500

    adjusted delete route

--- src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/springboot/cruddemo/rest/EmployeeRestController.java
@@ -56,7 +56,15 @@ public class EmployeeRestController {
     }
 
     @DeleteMapping("/employees/{employeeId}")
-    public void deleteById(@PathVariable int employeeId) {
+    public String deleteById(@PathVariable int employeeId) {
+        Employee employee = employeeService.findById(employeeId);
+
+        if (employee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
         employeeService.deleteById(employeeId);
+
+        return "Deleted employee id - " + employeeId;
     }
 }
