d0f1616703385c4c93cde41b9e6322e591e54c63
larry <ladams1776@yahoo.com>
2019-01-10 06:49:38 -0500

    injecting the new DAO that we are working on into the service and the api is working still, even with the new DAO for retrieving emps.

--- src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDaoJpaImpl.java
+++ src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDaoJpaImpl.java
@@ -1,16 +1,26 @@
 package com.luv2code.springboot.cruddemo.dao;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.List;
 
 @Repository
 public class EmployeeDaoJpaImpl implements EmployeeDAO {
 
+    private EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDaoJpaImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
     @Override
     public List<Employee> findAll() {
-        return null;
+        return (List<Employee>) entityManager.createQuery("from Employee").getResultList();
     }
 
     @Override
