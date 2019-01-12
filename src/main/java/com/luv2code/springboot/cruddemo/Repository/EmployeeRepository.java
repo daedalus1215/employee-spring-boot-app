63d13c1d9594e76e87cf2a229ad3797421b5b8a6
larry <ladams1776@yahoo.com>
2019-01-11 23:51:19 -0500

    added some pagination in properties and the RepositoryRestResource

--- src/main/java/com/luv2code/springboot/cruddemo/Repository/EmployeeRepository.java
+++ src/main/java/com/luv2code/springboot/cruddemo/Repository/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.luv2code.springboot.cruddemo.Repository;
 
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+
+//it is defaulted to /employees anyways, I just wanted to be explicit - we can change it to any end-route we want.
+@RepositoryRestResource(path="/employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
